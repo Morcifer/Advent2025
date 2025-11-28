a033e525e7fcec3bd7938bfd871547ba74b6c631
mor.verbin <mor.verbin@coolblue.nl>
2025-11-28 13:32:17 +0100

    Initial commit.

---
+++ src/utilities.py
@@ -0,0 +1,58 @@
+import os
+from typing import TypeVar, Callable
+
+ResultType = TypeVar('ResultType')
+
+
+def load_data(
+    day: int,
+    parser: Callable[[list[str]], ResultType] | Callable[[list[list[str]]], ResultType],
+    data_folder: str,
+    is_test: bool,
+    do_not_strip: bool = False,
+    cluster_at_empty_line: bool = False,
+) -> list[ResultType]:
+    # TODO: If file isn't there, go online.
+    file_name = (
+        os.path.join("..", data_folder, "test", f"day{day}_data.txt") if is_test
+        else os.path.join("..", data_folder, "real", f"day{day}_data.txt")
+    )
+
+    with open(file_name) as f:
+        content = f.readlines()
+
+    content = [x.replace("\n", "") if do_not_strip else x.strip() for x in content]
+
+    parsed_data = []
+    cluster = []
+
+    for line_number, line in enumerate(content):
+        s = line if do_not_strip else line.split(' ')
+        if not cluster_at_empty_line:
+            parsed_data.append(parser(s))
+            continue
+
+        cluster.append(s)
+
+        if line == "" or line_number == len(content) - 1:
+            parsed_data.append(parser(cluster))
+            cluster = []
+
+    return parsed_data
+
+
+def load_data_un_parsed(
+    day: int,
+    data_folder: str,
+    is_test: bool,
+    suffix: str = "",
+) -> list[ResultType]:
+    file_name = (
+        os.path.join("..", data_folder, "test", f"day{day}_data{suffix}.txt") if is_test
+        else os.path.join("..", data_folder, "real", f"day{day}_data{suffix}.txt")
+    )
+
+    with open(file_name) as f:
+        content = f.readlines()
+
+    return [x for x in content]
