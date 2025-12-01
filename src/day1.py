a7a4633e28f5991bce4a0077b5fb569ae14defa1
mor.verbin <mor.verbin@coolblue.nl>
2025-12-01 10:54:08 +0100

    Day 1: This year we try ascii art. Maybe.

--- src/day1.py
+++ src/day1.py
@@ -1,3 +1,5 @@
+import math
+
 from src.utilities import load_data
 
 DAY = 1
@@ -8,12 +10,54 @@ def parser(s: list[str]) -> tuple[chr, int]:
     return value[0], int(value[1:])
 
 
+def visualize(current: int, title: str) -> None:
+    scale_ratio = 1.75
+
+    rows = 21
+    columns = int(rows * scale_ratio)
+
+    radius = 9
+
+    screen = [[" "] * columns for _ in range(rows)]
+
+    x0 = columns // 2
+    y0 = rows // 2
+
+    for mark in range(100):
+        angle = (mark / 100.0 - 0.25) * 2.0 * math.pi
+
+        x1 = x0 + int(math.cos(angle) * radius * scale_ratio)
+        y1 = y0 + int(math.sin(angle) * radius)
+
+        if mark == 0:
+            screen[y1][x1] = "0"
+        else:
+            screen[y1][x1] = "#"
+
+    angle = (current / 100.0 - 0.25) * 2.0 * math.pi
+
+    for r in range(radius + 1):
+        x2 = x0 + int(math.cos(angle) * r * scale_ratio)
+        y2 = y0 + int(math.sin(angle) * r)
+
+        if current == 0:
+            screen[y2][x2] = "0"
+        else:
+            screen[y2][x2] = "C"
+
+    print(title)
+
+    for row in screen:
+        print("".join(row))
+
+
 def do_magic(data: list[tuple[chr, int]], collect_200_dollars: bool) -> int:
     result = 0
-
     current = 50
 
     for c, i in data:
+        visualize(current, f"from {current}, going {c} for {i} clicks, total {result}")
+
         previous = current
         extra_0s = 0
 
@@ -41,6 +85,8 @@ def do_magic(data: list[tuple[chr, int]], collect_200_dollars: bool) -> int:
 
         # print(f"Rotation {(c, i)} got from {previous} to {current} with {extra_0s} extra zeros")
 
+    visualize(current, f"Ending at {current}, total {result}")
+
     return result
 
 
@@ -60,6 +106,6 @@ def part(part_number: int, is_test: bool) -> int:
 
 
 if __name__ == "__main__":
-    TEST = False
+    TEST = True
     print(f"Day {DAY} result 1: {part(1, TEST)}")
     print(f"Day {DAY} result 2: {part(2, TEST)}")
