8ed4ac74b9a088945da51d9a09416f285231f8c0
mor.verbin <mor.verbin@coolblue.nl>
2025-12-04 09:09:47 +0100

    Day 4: Just because you don't *need* optimization doesn't mean you shouldn't do it. Sped up the loopy part by a factor of 33.

--- src/day4.py
+++ src/day4.py
@@ -26,18 +26,18 @@ neighbour_deltas = [
 ]
 
 
-def do_magic(data: list[ParsedType]) -> int:  # pylint: disable=unused-argument
+def calculate_adjacents(data: list[ParsedType]) -> dict[tuple[int, int], int]:
     height = len(data)
     width = len(data[0])
 
-    accessible = []
+    adjacents = {}
 
     for row in range(height):
         for column in range(width):
             if data[row][column] != "@":
                 continue
 
-            adjacent = 0
+            adjacents[(row, column)] = 0
 
             for delta in neighbour_deltas:
                 (neighbour_row, neighbour_column) = (row + delta[0], column + delta[1])
@@ -46,33 +46,84 @@ def do_magic(data: list[ParsedType]) -> int:  # pylint: disable=unused-argument
                     continue
 
                 if data[neighbour_row][neighbour_column] == "@":
-                    adjacent += 1
+                    adjacents[(row, column)] += 1
 
-            if adjacent < 4:
-                accessible.append((row, column))
+    return adjacents
+
+
+def do_magic_once(data: list[ParsedType]) -> int:  # pylint: disable=unused-argument
+    adjacents = calculate_adjacents(data)
+    accessible = [key for key, value in adjacents.items() if value < 4]
 
     for roll in accessible:
-        data[roll[0]][roll[1]] = "."
+        data[roll[0]][roll[1]] = "x"
 
     return len(accessible)
 
 
+def do_magic_efficiently_once(
+    data: list[ParsedType],
+    adjacents: dict[tuple[int, int], int] = None,
+) -> tuple[dict[tuple[int, int], int], int]:  # pylint: disable=unused-argument
+    height = len(data)
+    width = len(data[0])
+
+    if adjacents is None:
+        adjacents = calculate_adjacents(data)
+
+    rolls_to_remove = [key for key, value in adjacents.items() if value < 4]
+
+    for roll_to_remove in rolls_to_remove:
+        data[roll_to_remove[0]][roll_to_remove[1]] = "x"
+        adjacents.pop(roll_to_remove)
+
+        for delta in neighbour_deltas:
+            neighbour = (roll_to_remove[0] + delta[0], roll_to_remove[1] + delta[1])
+
+            if neighbour[0] < 0 or neighbour[0] >= height or neighbour[1] < 0 or neighbour[1] >= width:
+                continue
+
+            if neighbour in adjacents:
+                adjacents[neighbour] -= 1
+
+    return adjacents, len(rolls_to_remove)
+
+
+def do_magic(data: list[ParsedType], once: bool) -> int:
+    removed = do_magic_once(data)
+    result = removed
+
+    if once:
+        return result
+
+    while removed != 0:
+        logger.info("Still removing, result is %(result)s", {"result": result})
+        removed = do_magic_once(data)
+        result += removed
+
+    return result
+
+
+def do_magic_efficiently(data: list[ParsedType], once: bool) -> int:
+    adjacents, removed = do_magic_efficiently_once(data)
+    result = removed
+
+    if once:
+        return result
+
+    while removed != 0:
+        logger.info("Still removing, result is %(result)s", {"result": result})
+        adjacents, removed = do_magic_efficiently_once(data, adjacents)
+        result += removed
+
+    return result
+
+
 def part_1(is_test: bool) -> int:
     data = load_data(DAY, parser, "data", is_test=is_test)
-    return do_magic(data)
+    return do_magic_efficiently(data, once=True)
 
 
 def part_2(is_test: bool) -> int:
     data = load_data(DAY, parser, "data", is_test=is_test)
-
-    result = 0
-
-    removed = do_magic(data)
-    result += removed
-
-    while removed != 0:
-        logger.info("Still removing, result is %(result)s", {"result": result})
-        removed = do_magic(data)
-        result += removed
-
-    return result
+    return do_magic_efficiently(data, once=False)
