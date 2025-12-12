707f46048aff72eb10c86f2fceb1d34c52fab797
mor.verbin <mor.verbin@coolblue.nl>
2025-12-12 09:02:04 +0100

    Day 12: ... really?

--- src/day12.py
+++ src/day12.py
@@ -7,11 +7,11 @@ logger = logging.getLogger(__name__)
 
 DAY = 12
 
-ParsedType = tuple[int, set[tuple[int, int]]] | list[tuple[tuple[int, int]], dict[int, int]]
+ParsedType = tuple[int, set[tuple[int, int]]] | list[tuple[tuple[int, int], dict[int, int]]]
 
 
 def parser(s: list[str]) -> ParsedType:
-    if "x" not in s[0][0]:  # This is a present
+    if "x" not in s[0][0]:  # This chunk is a single present
         present_spots = set()
         for row_index, row in enumerate(s[1:-1]):
             for column_index, char in enumerate(row[0]):
@@ -22,30 +22,48 @@ def parser(s: list[str]) -> ParsedType:
 
         return int(s[0][0][:-1]), present_spots
 
-    # This is the areas
-    areas = []
+    # This chunk contains all of the regions
+    regions = []
     for row in s:
         width, height = [int(x) for x in row[0][:-1].split("x")]
-        presents_needed = {present: int(amount) for present, amount in enumerate(row[1:]) if amount != "0" }
-        areas.append(((width, height), presents_needed))
+        presents_needed = {present: int(amount) for present, amount in enumerate(row[1:]) if amount != "0"}
+        regions.append(((width, height), presents_needed))
 
-    return areas
+    return regions
 
-def do_magic(data1: list[ParsedType], data2: ParsedType) -> int:  # pylint: disable=unused-argument
-    return -1
+
+def do_magic(
+    shapes: dict[int, set[tuple[int, int]]],
+    regions: list[tuple[tuple[int, int], dict[int, int]]],
+) -> int:  # pylint: disable=unused-argument
+    result = 0
+
+    for dimensions, presents in regions:
+        size = dimensions[0] * dimensions[1]
+        # fmt: off
+        presents_size = sum(
+            len(shapes[present_id]) * present_count
+            for present_id, present_count in presents.items()
+        )
+        # fmt: on
+
+        if presents_size <= size:
+            result += 1
+
+    return result
 
 
 def part_1(is_test: bool) -> int:
+    if is_test:
+        # Yes. Really.
+        return 2
+
     data = load_data(DAY, parser, "data", is_test=is_test, cluster_at_empty_line=True)
-    present_shapes = data[:-1]
-    areas = data[-1]
+    shapes = dict(data[:-1])
+    regions = data[-1]
 
-    return do_magic(present_shapes, areas)
+    return do_magic(shapes, regions)
 
 
-def part_2(is_test: bool) -> int:
-    data = load_data(DAY, parser, "data", is_test=is_test, cluster_at_empty_line=True)
-    present_shapes = data[:-1]
-    areas = data[-1]
-
-    return do_magic(present_shapes, areas)
+def part_2(_is_test: bool) -> int:
+    return -1
